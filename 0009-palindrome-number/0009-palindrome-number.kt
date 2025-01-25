9876a66dda9265ffb13aa004bcf16a925c246767
Dlho <127379797+dlho1222@users.noreply.github.com>
2025-01-25 17:20:09 +0900

    Time: 5 ms (100%), Space: 37.8 MB (64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,3 +1,12 @@
 class Solution {
-    fun isPalindrome(x: Int): Boolean = x.toString() == x.toString().reversed()
-}
+        fun isPalindrome(x: Int): Boolean {
+            if (x < 0 || (x % 10 == 0 && x != 0)) return false
+            var num = x
+            var reversed = 0
+            while (num > reversed) {
+                reversed = reversed * 10 + num % 10
+                num /= 10
+            }
+            return num == reversed || num == reversed / 10
+        }
+    }
